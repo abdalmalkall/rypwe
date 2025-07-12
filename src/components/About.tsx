e620859fd441f24f860f02d6be644f04931ce4bf
abdalmalkall <abdalmalkall@example.com>
2025-07-12 19:28:20 +0300

    First full push with code and README

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,25 +1,20 @@
 const About = () => {
   return (
-<section id="about" className="py-20 bg-white flex items-center justify-center">
-
+    <section id="about" className="py-20 bg-white flex items-center justify-center">
       <div className="max-w-4xl mx-auto px-4 text-center">
         <h2 className="text-4xl md:text-5xl font-bold text-black mb-8">
           About <span className="text-gray-500">Ibrahim Alyan</span>
         </h2>
         <div className="space-y-6 text-gray-600 text-lg leading-relaxed">
-          
           <p>
-            
- is a architect and interior designer based in Jordan. He is the founder and creative lead behind RYP 
-              <h2 className="text-4xl md:text-1xl font-bold text-black mb-1">
-         <span className="text-gray-500"> (Render Your Plan)</span>
-        </h2> — a design brand specializing in architectural visualization and interior design solutions. With a strong academic foundation and hands-on experience in lighting and interior design firms in Amman, Ibrahim blends technical expertise with artistic vision to deliver bespoke, high-end spatial experiences.
+            is an architect and interior designer based in Jordan. He is the founder and creative lead behind 
+            <span className="font-bold text-black mx-1">RYP</span>
+                <span className="font-bold text-black mx-1">(Render Your Plan)</span>
+            — a design brand specializing in architectural visualization and interior design solutions. With a strong academic foundation and hands-on experience in lighting and interior design firms in Amman, Ibrahim blends technical expertise with artistic vision to deliver bespoke, high-end spatial experiences.
           </p>
           <p>
-   
-Driven by innovation and attention to detail, he brings clients’ visions to life through thoughtful planning, immersive 3D renderings, and functional yet elegant design concepts.
+            Driven by innovation and attention to detail, he brings clients’ visions to life through thoughtful planning, immersive 3D renderings, and functional yet elegant design concepts.
           </p>
-        
         </div>
       </div>
     </section>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
           <h1 className="text-5xl md:text-7xl font-bold mb-6">
          Ibrahim Alyan
           </h1>
-          <p className="text-xl md:text-2xl text-gray-300 mb-8 max-w-3xl mx-auto">
+          <p className="text-xl md:text-3xl text-gray-300 mb-8 max-w-xl mx-auto">
             Architect | Interior Designer | Founder of the RYP Brand
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
